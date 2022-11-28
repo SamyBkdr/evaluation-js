7a3b3ba73b1160612d040f5b860f78832eb2b0ff
SamyBkdr <samy.bkdr@gmail.com>
2022-11-28 12:50:50 +0100

    commitevaljs

---
+++ evaljs/script.js
@@ -0,0 +1,34 @@
+const slides = document.querySelectorAll(".Image");
+const nextSlide = document.querySelector(".btn-next");
+const prevSlide = document.querySelector(".btn-prev");
+
+let curSlide = 0;
+let maxSlide = slides.length - 1;
+
+slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${index * 100}%)`;
+});
+
+nextSlide.addEventListener("click", function () {
+    if (curSlide === maxSlide) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+    });
+});
+
+prevSlide.addEventListener("click", function () {
+    if (curSlide === 0) {
+        curSlide = maxSlide;
+    } else {
+        curSlide--;
+    }
+
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - curSlide)}%)`;
+    });
+});
